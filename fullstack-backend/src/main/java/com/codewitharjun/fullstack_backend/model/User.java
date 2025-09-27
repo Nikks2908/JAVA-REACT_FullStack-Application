250f7d73cc4501e322c4de4099929a6b0f2d3de8
Nikhil p <nikhilp408@gmail.com>
2025-09-27 13:31:14 +0530

    Initial commit

---
+++ fullstack-backend/src/main/java/com/codewitharjun/fullstack_backend/model/User.java
@@ -0,0 +1,46 @@
+package com.codewitharjun.fullstack_backend.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
